b595785e2063f986dfaa999d3504d15c86844ef7
Alan Richardson <alan@compendiumdev.co.uk>
2018-04-19 21:38:01 +0100

    tidied up test classes a little
    tidied up test classes a little

---
+++ java/src/test/java/com/seleniumsimplified/pulp/domain/AppEndPointTest.java
@@ -0,0 +1,82 @@
+package com.seleniumsimplified.pulp.domain;
+
+import com.seleniumsimplified.pulp.PulpApp;
+import com.seleniumsimplified.pulp.PulpData;
+import com.seleniumsimplified.pulp.reader.PulpDataPopulator;
+import com.seleniumsimplified.pulp.reader.SavageReader;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class AppEndPointTest {
+
+    @Test
+    public void canGetBooksByAuthor(){
+
+        PulpData books = new PulpData();
+        PulpDataPopulator populator = new PulpDataPopulator(books);
+        SavageReader reader = new SavageReader("/data/pulp/doc_savage_test.csv");
+        populator.populateFrom(reader);
+
+        PulpAuthor will = books.authors().findByName("Will Murray");
+
+        List<PulpBook> authored = books.books().findByAuthorId(will.getId());
+
+        Assert.assertEquals(1, authored.size());
+
+        PulpAuthor lester = books.authors().findByName("Lester Dent");
+        authored = books.books().findByAuthorId(lester.getId());
+        Assert.assertEquals(5, authored.size());
+    }
+
+    @Test
+    public void haveBasicAppWrapperForBooksByAuthor(){
+        PulpApp app = new PulpApp();
+        app.readSavageData("/data/pulp/doc_savage_test.csv");
+
+        PulpAuthor will = app.books().authors().findByName("Will Murray");
+
+        String report = app.reports().getBooksAsHtmlListWhereAuthor(will.getId());
+
+        System.out.println(report);
+        Assert.assertTrue(report.contains("<li>The Angry Canary"));
+
+        PulpAuthor lester = app.books().authors().findByName("Lester Dent");
+
+        report = app.reports().getBooksAsHtmlListWhereAuthor(lester.getId());
+
+        System.out.println(report);
+        Assert.assertTrue(report.contains("<li>The Angry Canary"));
+        Assert.assertTrue(report.contains("<li>Up From Earth's Center"));
+    }
+
+
+    @Test
+    public void haveBasicAppWrapperForBooksByPublisher(){
+        PulpApp app = new PulpApp();
+        app.readSavageData("/data/pulp/doc_savage_test.csv");
+
+        PulpPublisher pub = app.books().publishers().findByName("Street And Smith");
+
+        String report = app.reports().getBooksAsHtmlListWherePublisher(pub.getId());
+
+        System.out.println(report);
+        Assert.assertTrue(report.contains("<li>The Angry Canary"));
+
+    }
+
+    @Test
+    public void haveBasicAppWrapperForBooksBySeries(){
+        PulpApp app = new PulpApp();
+        app.readData( new SavageReader("/data/pulp/doc_savage_test.csv"));
+
+        PulpSeries series = app.books().series().findByName("Doc Savage");
+
+        String report = app.reports().getBooksAsHtmlListWhereSeries(series.getId());
+
+        System.out.println(report);
+        Assert.assertTrue(report.contains("<li>The Angry Canary"));
+
+    }
+}
---
+++ java/src/test/java/com/seleniumsimplified/pulp/domain/ToDoListTest.java
@@ -0,0 +1,36 @@
+package com.seleniumsimplified.pulp.domain;
+
+public class ToDoListTest {
+
+
+    // TODO: allow multiple filter params e.g. ?year=136&author=4
+
+
+    // TODO: Basic API
+    // TODO: add report classes to use for JSON and XML serialisation
+    // TODO: create a list of basic end points and add methods to support end point reporting
+    // TODO: add a basic REST API for get requests and reporting
+
+    // TODO: API Powered GUI
+    // TODO: add a simple GUI that uses the REST API for reporting the books
+    // TODO: add an AJAX based GUI that uses the REST API for reporting the books
+
+
+    // TODO: can READ and get without authentication
+    // TODO: add a single admin user hard coded
+    // TODO: can see an admin interface when logged in
+    // TODO: expand back end for delete, replace, partial amend - require admin user permissions and authentication
+    // TODO: books.authors().delete(1) - delete all books from that author
+    // TODO: add other users
+    // TODO: add permissions on entities, created-by, owner
+    // TODO: amendment based on permissions of user
+    // TODO: the static books reader and collection classes were created with a high level exploratory test - not low level TDD, really neeed class specific tests on the collections
+    //       20180415 bugs that slipped through because of this - hard coded paths, not reading books as unique books - i.e. every book was first in csv file
+
+    // TODO create unit tests for each of the pages and apps routings
+
+
+    // TODO: More Data
+    // TODO: Add books for others ()
+
+}
